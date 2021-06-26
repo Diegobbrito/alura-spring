d9a1db485ca34d634c68e96002ac95c4267b0e70
Diego Brito <dibbrito@gmail.com>
2021-06-26 15:59:28 -0300

    Add profiles for test and unit tests

---
+++ src/test/java/br/com/alura/forum/repository/CursoRepositoryTest.java
@@ -0,0 +1,44 @@
+package br.com.alura.forum.repository;
+
+import br.com.alura.forum.modelo.Curso;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.assertNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+@AutoConfigureTestDatabase( replace = AutoConfigureTestDatabase.Replace.NONE)
+@ActiveProfiles("test")
+public class CursoRepositoryTest {
+    
+    @Autowired
+    private CursoRepository repository;
+
+    @Autowired
+    private TestEntityManager em;
+
+    @Test
+    public void deveriaCarregarUmCursoAoBuscarPeloSeuNome() {
+        String nomeCurso = "HTML 5";
+        Curso curso = repository.findByNome(nomeCurso);
+        assertNotNull(curso);
+        assertEquals(nomeCurso, curso.getNome());
+    }
+
+    @Test
+    public void naoDeveriaCarregarUmCursoCujoNomeNaoEstejaCadastrado() {
+        String nomeCurso = "HTML 5";
+        Curso curso = repository.findByNome(nomeCurso);
+        assertNull(curso);
+    }
+}
